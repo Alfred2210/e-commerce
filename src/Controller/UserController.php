f8add6815ca133c15c251a43cec3519f41551038
Alfred2210 <alfredxavier.2210@gmail.com>
2023-01-29 19:51:25 +0100

    Complete Traduction of each pages

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
+#[Route('{_locale}')]
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
